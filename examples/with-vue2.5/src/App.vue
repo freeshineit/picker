f414323dc2aa9fa1aa4dc1e1623e64890b0bfd35
ShineShao <xiaoshaoqq@gmail.com>
2025-11-15 17:59:29 +0800

    demo: vue2.5

---
+++ examples/with-vue2.5/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <I18n />
+  </div>
+</template>
+
+<script>
+import I18n from "./components/i18n";
+export default {
+  name: "app",
+  components: {
+    I18n,
+  },
+};
+</script>
---
+++ examples/with-vue2.5/src/components/i18n/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div style="padding-top: 100px">
+    <button id="picker">picker</button>
+  </div>
+</template>
+
+<script>
+import '@skax/picker/dist/style/css.css';
+import Picker from '@skax/picker';
+
+let picker;
+
+export default {
+  name: 'Picker',
+  data() {
+    return {};
+  },
+  mounted: () => {
+    if (picker) {
+      picker.destroy();
+      picker = null;
+    }
+    picker = new Picker(() => document.getElementById('picker'), {
+      content: '<p>123</p>',
+      placement: 'bottom',
+      trigger: 'click',
+      onOpenChange: open => {
+        console.log('open:', open);
+      },
+    });
+
+    console.log(picker);
+  },
+  methods: {},
+};
+</script>
+<style lang="css">
+.epicker-body {
+  padding: 20px;
+  background-color: #fff;
+}
+</style>
