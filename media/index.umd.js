2799e240069a0421a5db95bb9742387d22360ce8
freeshineit <xiaoshaoqq@gmail.com>
2025-12-07 08:28:20 +0000

    Deploying to gh-pages from @ freeshineit/picker@2c29ca82c1fa8fed4412d4a6132594412507ce01 🚀

--- media/index.umd.js
+++ media/index.umd.js
@@ -2,7 +2,7 @@
  * picker component for js framework, support mobile and pc
  * 
  * @skax/picker v1.1.5
- * Copyright (c) 2025-11-15 ShineShao <xiaoshaoqq@gmail.com>
+ * Copyright (c) 2025-12-07 ShineShao <xiaoshaoqq@gmail.com>
  * 
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
