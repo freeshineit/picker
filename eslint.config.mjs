b2a58127d0636b52b03eb011eca8790049445465
ShineShao <xiaoshaoqq@gmail.com>
2026-04-14 00:39:51 +0800

    style: format code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@
 // import globals from 'globals';
 // import reactHooks from 'eslint-plugin-react-hooks';
 // import reactRefresh from 'eslint-plugin-react-refresh';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import configs from 'eslint-config-xx';
+import { defineConfig, globalIgnores } from "eslint/config";
+import configs from "eslint-config-xx";
 
 export default defineConfig([
-  globalIgnores(['dist', 'examples/with-base/index.umd.js']),
+  globalIgnores(["dist", "examples/with-base/index.umd.js"]),
   ...configs,
   // {
   //   files: ['**/*.{js,jsx}'],
