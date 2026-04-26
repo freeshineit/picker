3bd8b71e2e7eb50767255a673de0122d2f78dab0
freeshineit <xiaoshaoqq@gmail.com>
2026-04-26 15:34:08 +0000

    Deploying to gh-pages from @ freeshineit/picker@f8bb4306165225dcff53421fef970acef9531e84 🚀

--- media/index.umd.js
+++ media/index.umd.js
@@ -1,6 +1,6 @@
 /*
 * @skax/picker v2.0.0-beta.2
-* Copyright (c) 2026-04-25 ShineShao <xiaoshaoqq@gmail.com>
+* Copyright (c) 2026-04-26 ShineShao <xiaoshaoqq@gmail.com>
 * Released under the MIT License.
 */
 (function (global, factory) {
