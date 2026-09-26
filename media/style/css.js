8b597cfd1ede77c1498f4705cff904aa233ecc57
freeshineit <xiaoshaoqq@gmail.com>
2026-09-26 04:20:54 +0000

    Deploying to gh-pages from @ freeshineit/picker@97881264b880daa87dc3410642e52c0b257c05d3 🚀

--- media/style/css.js
+++ media/style/css.js
@@ -1,6 +1,6 @@
 /*
-* @skax/picker v2.0.0-beta.5
-* Copyright (c) 2026-09-23 18:19:07 ShineShao <xiaoshaoqq@gmail.com>
+* @skax/picker v2.0.0-beta.8
+* Copyright (c) 2026-09-26 04:20:34 ShineShao <xiaoshaoqq@gmail.com>
 * Released under the MIT License.
 */
 "use strict";
